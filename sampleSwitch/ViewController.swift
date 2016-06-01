842917894d6bcc4040c9c51093a690d44da90c78
TakayukiMorijiri <takayuki.m1206@gmail.com>
2016-06-01 22:23:19 +0900

    sampleSwitch

--- sampleSwitch/ViewController.swift
+++ sampleSwitch/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnSwitch: UISwitch!
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    //sender:このイベントを初どさせたオブジェクト
+    @IBAction func changeSwitch(sender: UISwitch) {
+        
+        print(sender.on)
+        
+        //TODO:スイッチがONの時はデバックエリアに「スイッチオン」、オフの時は「スイッチオフ」と表示しましょう
+        if sender.on == true{
+            print("スイッチオン")
+        }else{
+            print("スイッチオフ")
+        }
+    }
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
